c3131b78dc1c21005e9071b0e121512068566c2d
Jeremy Rustandi <jeremyandika21@gmail.com>
2021-09-23 21:57:45 +0700

    initial commit

---
+++ frontend/src/products.js
@@ -0,0 +1,83 @@
+const products = [
+    {
+      '_id': '1',
+      'name': 'Sapiens',
+      'image': '/images/sapiens.jpeg',
+      'description':
+        'Sapiens: A Brief History of Humankind is a book by Yuval Noah Harari, first published in Hebrew in Israel in 2011 based on a series of lectures Harari taught at The Hebrew University of Jerusalem, and in English in 2014',
+      'brand': 'Apple',
+      'category': 'Electronics',
+      'price': 89.99,
+      'countInStock': 0,
+      'rating': 4.5,
+      'numReviews': 12,
+    },
+    {
+      '_id': '2',
+      'name': 'Guns, Germs, and Steel',
+      'image': '/images/ggs.jpeg',
+      'description':
+        'Guns, Germs, and Steel: The Fates of Human Societies is a 1997 transdisciplinary non-fiction book by Jared Diamond. In 1998, Guns, Germs, and Steel won the Pulitzer Prize for general nonfiction and the Aventis Prize for Best Science Book',
+      'brand': 'Apple',
+      'category': 'Electronics',
+      'price': 599.99,
+      'countInStock': 7,
+      'rating': 4.0,
+      'numReviews': 8,
+    },
+    {
+      '_id': '3',
+      'name': 'Harry Potter Set',
+      'image': '/images/harrypotter.jpeg',
+      'description':
+        'Harry Potter is a series of seven fantasy novels written by British author J. K. Rowling. The novels chronicle the lives of a young wizard, Harry Potter, and his friends Hermione Granger and Ron Weasley, all of whom are students at Hogwarts School of Witchcraft and Wizardry',
+      'brand': 'Cannon',
+      'category': 'Electronics',
+      'price': 929.99,
+      'countInStock': 5,
+      'rating': 3,
+      'numReviews': 12,
+    },
+    {
+      '_id': '4',
+      'name': 'The Gunslinger',
+      'image': '/images/gunslinger.jpeg',
+      'description':
+        'The Gunslinger is a dark-fantasy by American author Stephen King. It is the first volume in the Dark Tower series. The Gunslinger was first published in 1982 as a fix-up novel, joining five short stories that had been published between 1978 and 1981',
+      'brand': 'Sony',
+      'category': 'Electronics',
+      'price': 399.99,
+      'countInStock': 11,
+      'rating': 5,
+      'numReviews': 12,
+    },
+    {
+      '_id': '5',
+      'name': 'The Intelligent Investor',
+      'image': '/images/investor.jpeg',
+      'description':
+        'The Intelligent Investor by Benjamin Graham, first published in 1949, is a widely acclaimed book on value investing. The book teaches readers strategies on how to successfully use value investing in the stock market.',
+      'brand': 'Logitech',
+      'category': 'Electronics',
+      'price': 49.99,
+      'countInStock': 7,
+      'rating': 3.5,
+      'numReviews': 10,
+    },
+    {
+      '_id': '6',
+      'name': 'How to win friends and influence people',
+      'image': '/images/influence.jpeg',
+      'description':
+        'How to Win Friends and Influence People is a self-help book written by Dale Carnegie, published in 1936. Over 30 million copies have been sold worldwide, making it one of the best-selling books of all time. Carnegie had been conducting business education courses in New York since 1912',
+      'brand': 'Amazon',
+      'category': 'Electronics',
+      'price': 29.99,
+      'countInStock': 0,
+      'rating': 4,
+      'numReviews': 12,
+    },
+  ]
+  
+  
+  export default products
