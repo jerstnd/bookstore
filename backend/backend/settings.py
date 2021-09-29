bd15625a3d00be3f8ba19e24fbce95c1796e26a9
Jeremy Rustandi <jeremyandika21@gmail.com>
2021-09-29 22:45:39 +0700

    modelling data

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -126,6 +126,13 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+STATICFILES_DIRS = [
+    BASE_DIR / 'static'
+]
+
+# tell django where to upload images
+MEDIA_ROOT = 'static/images'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
