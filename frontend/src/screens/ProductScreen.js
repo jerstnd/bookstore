0f81417b8102c682bf59f07a704299f0f149d7eb
jeremy <jeremyandika21@gmail.com>
2021-12-05 20:22:58 +0700

    implemented redux

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -16,7 +16,7 @@ function ProductScreen({match}) {
 
     useEffect(()=> {
         dispatch(listProductsDetails(match.params.id))
-    }, [])
+    }, [dispatch, match])
 
 
 
@@ -33,29 +33,29 @@ function ProductScreen({match}) {
                             <Col md={3}>
                                 <Image src={product.image} alt={product.name} />
                             </Col>
-                                
-                                
+
+
                             <Col md={6}>
                                 <ListGroup variant='flush'>
                                     <ListGroup.Item>
                                         <h3>{product.name}</h3>
                                     </ListGroup.Item>
-                                
+
                                     <ListGroup.Item>
                                         <Rating value={product.rating} text={`${product.numReviews} Reviews`} color={'#f8e825'} />
                                     </ListGroup.Item>
-                                
+
                                     <ListGroup.Item>
                                         <h5>Price: ${product.price}</h5>
                                     </ListGroup.Item>
-                                
+
                                     <ListGroup.Item>
                                         Description: {product.description}
                                     </ListGroup.Item>
                                 </ListGroup>
                             </Col>
-                                
-                                
+
+
                             <Col md={3}>
                                 <Card>
                                     <ListGroup variant='flush'>
@@ -65,14 +65,14 @@ function ProductScreen({match}) {
                                                 <Col><strong>${product.price}</strong></Col>
                                             </Row>
                                         </ListGroup.Item>
-                                
+
                                         <ListGroup.Item>
                                             <Row>
                                                 <Col>Stock:</Col>
                                                 <Col>{product.countInStock > 0 ? "In Stock" : "Out of Stock"}</Col>
                                             </Row>
                                         </ListGroup.Item>
-                                
+
                                         <ListGroup.Item>
                                             <Button className='w-100' disabled={product.countInStock == 0} type='button'>Add to Cart</Button>
                                         </ListGroup.Item>
